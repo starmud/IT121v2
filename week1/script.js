f82d2453480b75007dbff4c33331def49c31e0cf
starmud <45653563+starmud@users.noreply.github.com>
2025-01-14 04:24:03 +0000

    added

---
+++ week1/script.js
@@ -0,0 +1,7 @@
+// firstName, lastName, yearsOfStudy, and goal. //
+
+let firstName = "David";
+let lastName = "Charlebois";
+let yearsOfStudy = "2.5";
+let goal = "Web development & design";
+document.write("My name is " + firstName + " " + lastName + ", I've attended NSC & SCC for" + yearsOfStudy + " years. I plan to pursue " + goal + " after completing my studies.")
