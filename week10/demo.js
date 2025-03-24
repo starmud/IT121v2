5bd97b1a48edc992b5850a877ef469ff4254e981
David <45653563+starmud@users.noreply.github.com>
2025-03-24 02:31:32 -0700

    edit

---
+++ week10/demo.js
@@ -0,0 +1,17 @@
+let name = "david"
+let age = 35
+let isMale = true
+let height = 175.5
+let weight = 70.5
+let address = "123/456"
+let phone = "0123456789"
+let email = "boop@boop.com"
+
+console.log(`name: ${name}`)
+console.log(`age: ${age}`)
+console.log(`isMale: ${isMale}`)
+console.log(`height: ${height}`)
+console.log(`weight: ${weight}`)
+console.log(`address: ${address}`)
+console.log(`phone: ${phone}`)
+console.log(`email: ${email}`)
